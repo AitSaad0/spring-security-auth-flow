a2fd30d290ebbc1b93b020017cb20eaa1ac82793
Ait_SaaD <saadaitabd2004@gmail.com>
2025-11-29 18:36:39 +0100

    first commit

---
+++ src/main/java/com/example/spring_security_auth_flow/SpringSecurityAuthFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_auth_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_security_auth_flow/SpringSecurityAuthFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_auth_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
